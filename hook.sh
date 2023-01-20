4cc3d3cc121975cb2120336b9a9547fced45a00e
Cefatus <bt.sosei@gmail.com>
2023-01-20 18:05:14 +0700

    add ssh login
    Login to Github with passwordless deploy key.

--- hook.sh
+++ hook.sh
@@ -3,6 +3,16 @@
 # DIRECTORY TO THE REPOSITORY
 REPOSITORY="../repo"
 
+# This deploy key must not have challenge key.
+# Uncomment this, if your keys doesn't have default names.
+#SSH_KEY="/home/<user>/.ssh/<secret_key>"
+
+eval $(ssh-agent -s)
+ssh-add $SSH_KEY
+
+# test login just in case.
+ssh -T git@github.com
+
 cd $REPOSITORY
 
 git pull
